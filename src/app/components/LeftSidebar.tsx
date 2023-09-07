c8440a6d08301715143093e139fb079a2cf0c35c
Eric <erickimshkim@gmail.com>
2023-09-06 19:54:48 -0700

    add main section layout

---
+++ src/app/components/LeftSidebar.tsx
@@ -0,0 +1,74 @@
+import React from 'react'
+import Link from 'next/link'
+import {BiHomeCircle, BiUser} from 'react-icons/bi'
+import {BsBell, BsBookmark, BsTwitter, BsEnvelope, BsThreeDots} from 'react-icons/bs'
+import {HiOutlineHashtag} from 'react-icons/hi'
+
+const NAVIGATION_ICONS = [
+  {
+    title:'Twitter',
+    icon:BsTwitter
+  },
+  {
+    title:'Home',
+    icon:BiHomeCircle
+  },
+  {
+    title:'Explore',
+    icon:HiOutlineHashtag
+  },
+  {
+    title:'Notification',
+    icon:BsBell
+  },
+  {
+    title:'Messages',
+    icon:BsEnvelope
+  },
+  {
+    title:'Bookmarks',
+    icon:BsBookmark
+  },
+  {
+    title:'Profile',
+    icon:BiUser
+  }
+]
+
+const LeftSidebar = () => {
+  return (
+    <section className='fixed w-[275px] flex flex-col items-stretch h-screen p-6'>
+          <div className='flex flex-col items-stretch h-full space-y-4 mt-4'>
+          {NAVIGATION_ICONS.map((item)=>(
+            <Link className=' hover:bg-white/10 text-2xl transition duration-200 flex items-center justify-start w-fit space-x-4 rounded-full py-2 px-6' href={`/${item.title.toLowerCase()}`} key={item.title}>
+              <div>
+                <item.icon/>
+              </div>
+              <div>
+                {item.title !== 'Twitter' && <div>{item.title}</div>}
+              </div>
+            </Link>
+            ))}
+            <button className='rounded-full bg-primary m-4 px-4 py-2 text-xl text-center hover:bg-opacity-70 transition duration-200'>
+              Tweet
+            </button>
+          </div>
+          <div>
+            <button className='rounded-full flex items-center space-x-2 bg-transparent m-4 p-4 text-center hover:bg-white/10 transition duration-200 w-full justify-between'>
+              <div className='flex items-center space-x-2'>
+                <div className='rounded-full bg-slate-400 w-10 h-10'></div>
+                <div className='text-left text-sm'>
+                  <div className='font-semibold'>User Name</div>
+                  <div className=''>@username</div>
+                </div>
+              </div>
+              <div>
+                <BsThreeDots/>
+              </div>
+            </button>
+          </div>
+    </section>
+  )
+}
+
+export default LeftSidebar
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
 import Link from 'next/link'
 import React from 'react'
 import {BiHomeCircle, BiUser} from 'react-icons/bi'
-import {BsBell, BsBookmark, BsTwitter, BsEnvelope, BsThreeDots} from 'react-icons/bs'
+import {BsBell, BsBookmark, BsTwitter, BsEnvelope, BsThreeDots, BsChat, BsDot} from 'react-icons/bs'
 import {HiOutlineHashtag} from 'react-icons/hi'
+import {AiOutlineHeart, AiOutlineRetweet} from 'react-icons/ai'
+import {IoShareOutline, IoStatsChart} from 'react-icons/io5'
+import LeftSidebar from './components/LeftSidebar'
 
 const NAVIGATION_ICONS = [
   {
@@ -41,39 +44,79 @@ const Home = () => {
     <div className='w-full h-full flex justify-center items-center relative bg-black'>
       <div className='max-w-screen-xl w-full h-full flex relative'>
         {/* left sidebar for navigation/header */}
-        <section className='fixed w-[275px] flex flex-col items-stretch h-screen'>
-          <div className='flex flex-col items-stretch h-full space-y-4 mt-4'>
-          {NAVIGATION_ICONS.map((item)=>(
-            <Link className=' hover:bg-white/10 text-2xl transition duration-200 flex items-center justify-start w-fit space-x-4 rounded-full py-2 px-6' href={`/${item.title.toLowerCase()}`} key={item.title}>
-              <div>
-                <item.icon/>
-              </div>
-              <div>
-                {item.title !== 'Twitter' && <div>{item.title}</div>}
-              </div>
-            </Link>
-            ))}
-            <button className='rounded-full bg-primary m-4 p-4 text-2xl text-center hover:bg-opacity-70 transition duration-200'>
-              Tweet
-            </button>
-          </div>
-          <div>
-            <button className='rounded-full flex items-center space-x-2 bg-transparent m-4 p-4 text-center hover:bg-white/10 transition duration-200 w-full justify-between'>
-              <div className='flex items-center space-x-2'>
-                <div className='rounded-full bg-slate-400 w-10 h-10'></div>
-                <div className='text-left text-sm'>
-                  <div className='font-semibold'>User Name</div>
-                  <div className=''>@username</div>
+        <LeftSidebar/>
+        <main className='ml-[275px] flex h-full w-full max-w-[600px] min-h-screen flex-col border-l-[0.5px] border-r-[0.5px] border-gray-600'>
+          <h1 className='text-xl font-bold p-6 backdrop-blur bg-black/10 sticky top-0'>Home</h1>
+          <div className='border-t-[0.5px] border-b-[0.5px] px-4 flex items-stretch py-6 space-x-2 border-gray-600 relative'>
+            <div className='w-10 h-10 bg-slate-400 rounded-full flex-none'></div>
+            <div className='flex flex-col w-full'>
+              <input 
+                type='text' 
+                className='w-full h-full rounded-full placeholder:text-2xl placeholder:text-gray-600 bg-transparent border-b-[0.5px] border-gray-600 p-4 outline-none border-none' 
+                placeholder="What's happening?"/>
+              <div className='w-full justify-between items-center flex'>
+                <div>
+
+                </div>
+                <div className='w-full max-w-[100px]'>
+                  <button className='rounded-full bg-primary px-4 py-2 w-full text-lg text-center hover:bg-opacity-70 transition duration-200'>
+                    Tweet
+                  </button>
                 </div>
               </div>
-              <div>
-                <BsThreeDots/>
-              </div>
-            </button>
+            </div>
           </div>
-        </section>
-        {/*<main>Home timeline</main>
-        <section>right section</section>*/}
+          <div className='flex flex-col'>
+    {
+      Array.from({length:5}).map((_,i)=> (
+        <div 
+          key={i} 
+          className='border-b-[0.5px] p-2 flex space-x-4 border-gray-600'>
+          <div>
+            <div className='w-10 h-10 bg-slate-200 rounded-full'/>
+          </div>
+          <div className='flex flex-col'>
+            <div className='flex items-center justify-between'>
+            <div className='flex items-center space-x-1 w-full'>
+              <div className='font-bold'>User 1</div>
+              <div className='text-gray-500'>@user1</div>
+              <div className='text-gray-500'><BsDot/></div>
+              <div className='text-gray-500'>1 hour ago</div>
+            </div>
+            <div>
+              <BsThreeDots/>
+            </div>
+            </div>
+            <div className='text-white text-base'>
+              Blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah
+            </div>
+            <div className='bg-slate-400 aspect-square w-full h-80 rounded-xl mt-2'>
+
+            </div>
+            <div className='flex items-center justify-start space-x-20 mt-2 w-full'>
+              <div className='rounded-full hover:bg-white/10 transition duration-200 p-3 cursor-pointer'>
+                <BsChat/>
+              </div>
+              <div className='rounded-full hover:bg-white/10 transition duration-200 p-3 cursor-pointer'>
+                <AiOutlineRetweet/>
+              </div>
+              <div className='rounded-full hover:bg-white/10 transition duration-200 p-3 cursor-pointer'>
+                <AiOutlineHeart/>
+              </div>
+              <div className='rounded-full hover:bg-white/10 transition duration-200 p-3 cursor-pointer'>
+                <IoStatsChart/>
+              </div>
+              <div className='rounded-full hover:bg-white/10 transition duration-200 p-3 cursor-pointer'>
+                <IoShareOutline/>
+              </div>
+            </div>
+          </div>
+        </div>
+      ))
+    }
+          </div>
+        </main>
+        {/*<section>right section</section>*/}
       </div>
     </div>
   )
